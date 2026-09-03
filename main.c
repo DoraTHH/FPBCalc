71773259a4deae6d3f554a00435b0526f1a014c6
DoraTHH <erikerikssonsepost@gmail.com>
2026-09-03 14:34:54 +0200

    made it accept command line input instead of being a CLI

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //Checks if FPB is an integer for every FPS & BPM combination from FPS == 1 to FPS == BPM
 
 void calculate(int BPM) { 
     int FPS = 1;
     int framesPerBeat = 0;
-    char FPBstring[] = "FPB";
-
-    if(BPM) //This if statement prevents the print from happening if the user wishes to exit the program
-        printf("%-8s FPS\n", FPBstring);
 
     while (FPS < BPM) //FPS can arbitrarily not go over BPM, there's probably a better way to set the range
     {
@@ -21,12 +18,9 @@ void calculate(int BPM) {
     return;
 }
 
-int main() {
-    int BPM;
-    while(BPM) {
-        printf("Enter 0 to exit or enter BPM: ");
-        scanf("%d", &BPM);
-        calculate(BPM);
-    }
+int main(int argc, char* argv[]) {
+    int BPM = atoi(argv[1]);
+    calculate(BPM);
+    printf("program has finished, exiting...");
     return 0;
 }
